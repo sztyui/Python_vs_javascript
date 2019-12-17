1a6e7c002dd408f727da2650c27821f6558d9aa1
Sztyui <6323pityu@citromail.hu>
2019-12-17 17:29:53 +0100

    Variables 2

--- src/javascript/js_python_tutor_variables.js
+++ src/javascript/js_python_tutor_variables.js
@@ -1,24 +1,46 @@
-function sayHi(){
-    console.log('Hi ' + myName);
-}
+// function sayHi(){
+//     myName = "Sally";
+//     console.log('Hi ' + myName);
+// }
 
-var myName = "Isti";
-var myLastName = "Szilágyi";
-var myAge = 29;
-var myArray = [1, 2, 3, 4, 5, myName, myLastName];
-var myObject = {
-    firstName: "Isti",
-    age: 29,
-    lastName: "Szilágyi",
-    colour: "black",
-}
+// var myName = "Isti";
+// var myLastName = "Szilágyi";
+// var myAge = 29;
+// var myArray = [1, 2, 3, 4, 5, myName, myLastName];
+// var myObject = {
+//     firstName: "Isti",
+//     age: 29,
+//     lastName: "Szilágyi",
+//     colour: "black",
+// }
 
-console.log(typeof myName);
-console.log(myLastName);
-console.log(myAge);
-console.log(myArray);
-console.log(typeof myObject);
+// console.log(typeof myName);
+// console.log(myLastName);
+// console.log(myAge);
+// console.log(myArray);
+// console.log(typeof myObject);
 
-sayHi();
-myName = "Jane";
-sayHi();
+// sayHi();
+// myName = 30;
+// sayHi();
+
+// var count = 1;
+
+// function increase(){
+//     count += 1;
+//     console.log(count);
+// }
+
+// increase();
+// increase();
+// increase();
+
+// var myNAME = "Chris";
+// console.log(myname);
+
+var myName = "Chris"
+var amCrhis = (myName === "Chris") ? true : false;
+var amINull;
+
+console.log(amCrhis);
+console.log(amINull);
