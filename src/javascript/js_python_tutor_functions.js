6f666d423c74a769de696ac651440abf63533c13
Sztyui <6323pityu@citromail.hu>
2020-02-03 20:03:40 +0100

    Non primitive properties

---
+++ src/javascript/js_python_tutor_functions.js
@@ -0,0 +1,56 @@
+// Functions
+/*
+    "A function is a JavaScript procedure - a set of statements
+    that performs a task or calculates a value. [...]"
+    - MDN
+*/
+
+/* function sayHi(user) {
+    console.log('Hi ' + user.firstName + " " + user.lastName + "!");
+};
+
+function numberDuble(num){
+    return num * 2;
+}
+
+function numberAdder(num1, num2){
+    return num1 + num2;
+}
+
+var bob = {firstName: "Bob", lastName: "Bjúkennen"};
+var brike = {firstName: "Brájk", lastName: "Szilent"};
+var klubb = {firstName: "Klubb", lastName: "Korona"};
+
+sayHi(bob);
+sayHi(brike);
+sayHi(klubb);
+
+console.log(numberDuble(10));
+
+const numTwoDoubled = numberDuble(2);
+const numFiveDoubled = numberDuble(5);
+console.log(numTwoDoubled);
+console.log(numFiveDoubled);
+console.log(numberAdder(numTwoDoubled, numFiveDoubled));
+
+function sayHi() {
+    if(!myName){
+        var myName = 'Bob';
+    }
+    console.log("Hi " + myName);
+}
+
+const myName = "Chirs";
+
+sayHi(); */
+
+// const squaredNumber = function (num) {
+//     return num * num;
+// }
+
+// const numberAdder = function (num1, num2, task) {
+//     return task(num1 + num2);
+// }
+
+// console.log(numberAdder(2, 3, squaredNumber));
+
