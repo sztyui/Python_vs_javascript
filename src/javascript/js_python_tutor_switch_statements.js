fc20d3bc85673eafadb67dc080bfdbf30f48a7f8
Sztyui <6323pityu@citromail.hu>
2020-03-13 20:30:37 +0100

    First switch expression

---
+++ src/javascript/js_python_tutor_switch_statements.js
@@ -0,0 +1,13 @@
+// Switch statements
+
+expression = "apple";
+switch (expression){
+    case "apple":
+        console.log("Awesome! I love apples.");
+        break;
+    case "orange":
+        console.log("Oranges are cool. :D");
+        break;
+    default:
+        console.log("That sounds pretty OK.");
+}
