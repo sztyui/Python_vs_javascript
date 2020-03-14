0c90307a07f44c4c4a6471beca8050839851aea0
Sztyui <6323pityu@citromail.hu>
2020-03-14 20:29:10 +0100

    Continue

--- src/javascript/loops.js
+++ src/javascript/loops.js
@@ -1,15 +1,18 @@
 // Loops
 // Quick and easy was to do something repeatedly
 
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
     if(!(i%3) && !(i%5)){
-        console.log("FizzBuzz");
+        // console.log("FizzBuzz");
+        continue;
     }
     else if(!(i%3)){
-        console.log("Fizz");
+        // console.log("Fizz");
+        continue;
     }
     else if(!(i%5)){
-        console.log("Buzz");
+        // console.log("Buzz");
+        continue;
     }
     else {
         console.log(i);
