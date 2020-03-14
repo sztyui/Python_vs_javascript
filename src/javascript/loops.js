04b768c8bb6bd66990b84441aed944ea0eb7cbd8
Sztyui <6323pityu@citromail.hu>
2020-03-14 22:23:57 +0100

    Christmas tree with loops.

--- src/javascript/loops.js
+++ src/javascript/loops.js
@@ -1,20 +1,24 @@
 // Loops
 // Quick and easy was to do something repeatedly
 
-for(let i = 1; i <= 100; i++){
-    if(!(i%3) && !(i%5)){
-        // console.log("FizzBuzz");
-        continue;
-    }
-    else if(!(i%3)){
-        // console.log("Fizz");
-        continue;
-    }
-    else if(!(i%5)){
-        // console.log("Buzz");
-        continue;
-    }
-    else {
-        console.log(i);
-    }
+const range = (start, end) => {
+    const length = end - start;
+    return Array.from({length}, (_, i) => start + i);
+}
+
+let x = 1, akt = 1, row = 26, jel = '*';
+szorzo = (row - akt)/2;
+while(szorzo > 0){
+    ures = ' '.repeat(szorzo);
+    teli = jel.repeat(akt);
+    console.log(`${ures}${teli}`);
+    akt += 2;
+    szorzo = (row - akt)/2;
+    x += 1;
+}
+lab = (row - 3)/2;
+for(_ in range(0,2)){
+    ures = ' '.repeat(lab);
+    teli = jel.repeat(3);
+    console.log(`${ures}${teli}`);
 }
