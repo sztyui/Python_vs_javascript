81a59b6ff95330691776c53e274c670b5ecb7d37
Sztyui <6323pityu@citromail.hu>
2020-03-14 09:49:41 +0100

    While loops first

---
+++ src/javascript/loops.js
@@ -0,0 +1,8 @@
+// Loops
+// Quick and easy was to do something repeatedly
+
+let numberOfLoops = 0;
+while(numberOfLoops < 10) {
+    console.log("I am looping: " + (numberOfLoops + 1) + " times.");
+    numberOfLoops += 1;
+}
