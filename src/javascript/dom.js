62fd6a63261aca0daee5960c27ae5a6e95215710
Sztyui <6323pityu@citromail.hu>
2020-03-18 20:49:02 +0100

    GetElementById

--- src/javascript/dom.js
+++ src/javascript/dom.js
@@ -1,17 +1,1 @@
-
-const myTag = document.getElementsByTagName('p');
-
-let aNagyIgazság = "Fuhhh gyerekek...";
-
-const elsoaldozas = function(){
-    if(myTag[3].innerHTML === aNagyIgazság){
-        myTag[3].innerHTML = "A gyász a picsátokat kieszi";
-    } else{
-        myTag[3].innerHTML = aNagyIgazság;
-    }
-    
-}
-
-for(const p of myTag){
-    console.log(p.innerHTML);
-}
+document.getElementById("identifier").innerHTML = "<h1>We all</h1>";
