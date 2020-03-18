9ffe3796e53f63e39db8b19f1205061c89641998
Sztyui <6323pityu@citromail.hu>
2020-03-18 19:56:48 +0100

    Bratan szavainak oldala.

--- src/javascript/dom.js
+++ src/javascript/dom.js
@@ -1,3 +1,18 @@
-let showAlert = () => {
-    window.alert("I'm alerting you to the fact that the body has been loaded.");
+
+const myTag = document.getElementsByTagName('p');
+
+let aNagyIgazság = "Fuhhh gyerekek...";
+
+const elsoaldozas = function(){
+    if(myTag[3].innerHTML === aNagyIgazság){
+        myTag[3].innerHTML = "A gyász a picsátokat kieszi";
+    } else{
+        myTag[3].innerHTML = aNagyIgazság;
+    }
+    
 }
+
+Array.prototype.forEach.call(myTag, function(element){
+    console.log(element.innerHTML);
+});
+
