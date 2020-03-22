740b2054f5cc06b57db894f990ba400a219b444c
Sztyui <6323pityu@citromail.hu>
2020-03-22 11:49:25 +0100

    Finished removing elements.

--- src/javascript/dom.js
+++ src/javascript/dom.js
@@ -29,6 +29,7 @@ creationDiv.appendChild(newElement);
 const allatokDiv = document.getElementById("allatok");
 let allatok = ["kutya", "cica", "bari", "kecske", "17 kilós ronda BAKNYÚL"];
 const ul = document.createElement("ul");
+ul.id = "allatLista";
 for(const allat of allatok){    
     let newAllat = document.createElement("li");
     newAllat.innerText = allat;
@@ -43,7 +44,7 @@ document.body.appendChild(allatokDiv);
 const nagyBaknyul = document.querySelector('.pink');
 console.log(nagyBaknyul);
 setInterval(function(){
-    nagyBaknyul.classList.toggle("pink");
+    nagyBaknyul.classList.toggle("pinkreverse");
 }, 1000);
 
 
@@ -52,10 +53,14 @@ newElement2.id = "hatalmasACsekom";
 document.body.appendChild(newElement2);
 document.body.removeChild(newElement2);
 
-
-const erdeiAllatok = allatokDiv.getElementsByTagName("li");
-for(let allatka of erdeiAllatok){
-    if(allatka.innerText === "bari"){
-        ul.removeChild(allatka);
+const removeFuckingBari = function(){
+    const ul = document.getElementById("allatLista");
+    const erdeiAllatok = allatokDiv.getElementsByTagName("li");
+    for(let allatka of erdeiAllatok){
+        if(allatka.innerText === "bari"){
+            ul.removeChild(allatka);
+        }
     }
 }
+
+removeFuckingBari();
